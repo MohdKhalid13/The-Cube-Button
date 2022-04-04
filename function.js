a347766a472e7fe6e1af903ccf9ff567fd51140c
mohdkhalid.mansoori@gmail.com <mohdkhalid.mansoori@gmail.com>
2022-04-04 22:01:53 +0530

    Cube Button

---
+++ function.js
@@ -0,0 +1,12 @@
+
+
+function hi(){
+    a = prompt("Enter a Number : ")
+    b = a*a*a;
+    document.getElementById('text').value += b
+
+}
+
+function hello(){
+    document.getElementById('text').value = ''
+}
